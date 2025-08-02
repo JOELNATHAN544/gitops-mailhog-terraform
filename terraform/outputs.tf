1dfbc159f3530a8c53f9449b1a8d34c8b779e584
Jagoum Richmond <jagoum24@gmail.com>
2025-08-02 12:06:04 +0100

    chore: added some few files and changed some things

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "argocd_server_url" {
-  description = "Argo CD server URL"
-  value       = data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].hostname
+output "argocd_server_node_port" {
+  description = "Argo CD server NodePort"
+  value       = data.kubernetes_service.argocd_server.spec[0].port[0].node_port
 }
