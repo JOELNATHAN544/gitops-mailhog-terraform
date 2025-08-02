f593c23991e72f8f93825717bef61efd4b35f6ad
Jagoum Richmond <jagoum24@gmail.com>
2025-08-02 10:24:18 +0100

    chore: populated the files for terraform

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -13,3 +13,14 @@ resource "helm_release" "argocd" {
     kubernetes_namespace.argocd
   ]
 }
+
+data "kubernetes_service" "argocd_server" {
+  metadata {
+    name      = "argocd-server"
+    namespace = kubernetes_namespace.argocd.metadata[0].name
+  }
+
+  depends_on = [
+    helm_release.argocd
+  ]
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "argocd_server_url" {
   description = "Argo CD server URL"
-  value       = helm_release.argocd.status.load_balancer_ingress[0].hostname
+  value       = data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].hostname
 }
